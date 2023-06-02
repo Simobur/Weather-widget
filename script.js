92bc2851ad88a10bef78312a6f6769b5ad441e8d
Simobur <burekszymon.biuro@gmail.com>
2023-06-02 15:38:45 +0200

    search button

--- script.js
+++ script.js
@@ -3,18 +3,33 @@ const btnSearch = document.querySelector(".btn-search-icon");
 const searchWindow = document.querySelector(".search-window");
 const container = document.querySelector(".container");
 const input = document.querySelector(".input");
+const box = document.querySelector(".box");
 
 btnOpen.addEventListener("click", function () {
   searchWindow.classList.toggle("hidden");
   btnOpen.classList.toggle("hidden");
   setTimeout(function () {
     input.focus();
-  }, 500);
-
-  console.log(input);
+  }, 200);
 });
 
 btnSearch.addEventListener("click", function () {
   searchWindow.classList.toggle("hidden");
   btnOpen.classList.toggle("hidden");
+});
+
+document.addEventListener("keydown", function (e) {
+  console.log(e);
+  if (e.key === "Escape") {
+    searchWindow.classList.add("hidden");
+    btnOpen.classList.remove("hidden");
+    input.value = "";
+  }
+});
+
+let cityName;
+
+window.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(e);
 });
